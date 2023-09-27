43cc20169f911243ade7c3e0fba816ddee876040
OPGCoding <97985921+OPGCoding@users.noreply.github.com>
2023-09-27 12:40:06 +0200

    Terningekast.cpp

--- Terningekast/Terningekast.cpp
+++ Terningekast/Terningekast.cpp
@@ -1,32 +1,64 @@
-/* Rød / Svær: 
-Lav et program hvor man kan indtaste hvor mange terninger der skal kastes, og få et resultat der giver mening.Ekstra
-Sortér resultaterne i numerisk rækkefølge.*/
+/* RÃ¸d / SvÃ¦r: 
+Lav et program hvor man kan indtaste hvor mange terninger der skal kastes, og fÃ¥ et resultat der giver mening.Ekstra
+SortÃ©r resultaterne i numerisk rÃ¦kkefÃ¸lge.*/
 
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <time.h>
-#include <algorithm>
+#include <ctime>
 
 using namespace std;
 
-int main()
-{
-	vector<int> RandomNumberVec;
-	int Terningekast;
-	cout << "Hvor mange terninger vil du kaste: ";
-	cin >> Terningekast;
-
-	cout << "Du kastede: " << Terningekast << " terninger. Du slog: ";
-
-	for (int i = 0; i < Terningekast; i++) {
-		int RandomNumber = (rand() % 6 + 1);
-		RandomNumberVec.push_back(RandomNumber);
-	}
-
-	sort(RandomNumberVec.begin(), RandomNumberVec.end());
-	for (int res : RandomNumberVec) {
-		cout << res << " ";
-	}
-	
+// Funktion til at bytte to elementer i en vektor
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
+
+// Funktion til at udfÃ¸re Selection Sort pÃ¥ en vektor
+void selectionSort(vector<int>& vec) {
+    int n = vec.size();
+
+    for (int i = 0; i < n - 1; i++) {
+        int min_index = i;
+
+        for (int j = i + 1; j < n; j++) {
+            if (vec[j] < vec[min_index]) {
+                min_index = j;
+            }
+        }
+
+        swap(vec[i], vec[min_index]);
+    }
+}
+
+int main() {
+    vector<int> RandomNumberVec;
+    int Terningekast;
+    
+    cout << "Hvor mange terninger vil du kaste: ";
+    cin >> Terningekast;
+
+    cout << "Du kastede: " << Terningekast << " terninger. Du slog: ";
+
+    // InitialisÃ©r random seed baseret pÃ¥ tid
+    srand(time(nullptr));
+
+    for (int i = 0; i < Terningekast; i++) {
+        int RandomNumber = (rand() % 6 + 1);
+        RandomNumberVec.push_back(RandomNumber);
+    }
+
+    // Brug selectionSort-funktionen til at sortere vektoren
+    selectionSort(RandomNumberVec);
+
+    for (int res : RandomNumberVec) {
+        cout << res << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
+
